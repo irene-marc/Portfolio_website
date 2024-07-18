b519a55698da7a9ac12e2a5413ac9055bfb98988
EdoardoMarchetti <62024316+EdoardoMarchetti@users.noreply.github.com>
2024-07-18 21:19:09 +0200

    education experience added

--- pages_script/home_page.py
+++ pages_script/home_page.py
@@ -37,7 +37,6 @@ def sort_by_dates(dicts):
 def compute_duration(start_str, end_str):
     start_date = parse_date(start_str)
     end_date = parse_date(end_str)
-    print(start_date, end_date)
     total_months = (end_date.year - start_date.year) * 12 + end_date.month - start_date.month
 
     years = total_months // 12
@@ -114,31 +113,87 @@ work_experince = read_json(osp.join('working_experience.json'))
 # Sort the list
 work_experince = sort_by_dates(work_experince)
 
-for we in work_experince:
-    st.markdown('<br>', unsafe_allow_html=True)
-    img_col, text_col = st.columns([0.1,0.9], gap='medium')
+for i, we in enumerate(work_experince):
+    #st.markdown('<br>', unsafe_allow_html=True)
+    with sc(key=f'exp_{i}', css_styles=["""div[data-testid="stHorizontalBlock"]:first-of-type {
+                                        border: 1px solid #335384;
+                                        border-radius: 20px;
+                                        padding:15px;
+                                        padding-top:30px;
+                                        box-shadow: rgb(0 0 0 / 20%) 0px 2px 1px -1px, rgb(0 0 0 / 14%) 0px 1px 1px 0px, rgb(0 0 0 / 12%) 0px 1px 3px 0px;
+    }
+    """]):
+        with st.container():
+            img_col, text_col = st.columns([0.1,0.9], gap='medium', vertical_alignment='top')
 
-    with img_col:
-        st.image(we['media']['path'])
-    
-    with text_col:
-        start_transformed = transform_date(we['start'])
-        end_transformed = transform_date(we['end'])
-        we['display_period'] = f"{start_transformed} - {end_transformed}"
+            with img_col:
+                with sc(key=f'exp_img_{i}', css_styles=["""img{
+                                        margin-top:15px;                                        
+                }
+                """]):
+                    st.image(we['media']['path'])
+            
+            with text_col:
+                start_transformed = transform_date(we['start'])
+                end_transformed = transform_date(we['end'])
+                we['display_period'] = f"{start_transformed} - {end_transformed}"
 
-        duration = compute_duration(we['start'], we['end'])
+                duration = compute_duration(we['start'], we['end'])
 
-        st.markdown(f"<span style='color:{LIGHT_GRAY}; font-weight:bold; white-space: pre;'>{we['company']}</span>  |   <span style='color:{LIGHT_GRAY};'>{we['display_period']} ({duration})</span>", unsafe_allow_html=True)
-        st.markdown(f"### {we['name']}")
-        st.markdown(f"{we['description']}")
-        if we['hard_skills']:
-            st.markdown(f"**Hard skills** : {', '.join(we['hard_skills'])}")
-        if we['soft_skills']:
-            st.markdown(f"**Soft skills** : {', '.join(we['soft_skills'])}")
-        if we['related_projects']:
-            st.markdown(f"**Related Projects** : {', '.join(we['related_projects'])}")
-        
-        st.markdown("<br>", unsafe_allow_html=True)
+                st.markdown(f"<span style='color:{LIGHT_GRAY}; font-weight:bold; white-space: pre;'>{we['company']}</span>  |   <span style='color:{LIGHT_GRAY};'>{we['display_period']} ({duration})</span>", unsafe_allow_html=True)
+                st.markdown(f"### {we['name']}")
+                st.markdown(f"{we['description']}")
+                if we['hard_skills']:
+                    st.markdown(f"**Hard skills** : {', '.join(we['hard_skills'])}")
+                if we['soft_skills']:
+                    st.markdown(f"**Soft skills** : {', '.join(we['soft_skills'])}")
+                if we['related_projects']:
+                    st.markdown(f"**Related Projects** : {', '.join(we['related_projects'])}")
+                
+                st.markdown("<br>", unsafe_allow_html=True)
+
+
+st.markdown('## 🧑‍🎓 Education')
+
+education = read_json(osp.join('education.json'))
+
+# Sort the list
+education = sort_by_dates(education)
+
+for i, e in enumerate(education):
+    #st.markdown('<br>', unsafe_allow_html=True)
+    with sc(key=f'e_{i}', css_styles=["""div[data-testid="stHorizontalBlock"]:first-of-type {
+                                        border: 1px solid #335384;
+                                        border-radius: 20px;
+                                        padding:15px;
+                                        padding-top:30px;
+                                        box-shadow: rgb(0 0 0 / 20%) 0px 2px 1px -1px, rgb(0 0 0 / 14%) 0px 1px 1px 0px, rgb(0 0 0 / 12%) 0px 1px 3px 0px;
+    }
+    """]):
+        with st.container():
+            img_col, text_col = st.columns([0.1,0.9], gap='medium', vertical_alignment='top')
+
+            with img_col:
+                with sc(key=f'exp_img_{i}', css_styles=["""img{
+                                        margin-top:15px;                                        
+                }
+                """]):
+                    st.image(e['media']['path'])
+            
+            with text_col:
+                start_transformed = transform_date(e['start'])
+                end_transformed = transform_date(e['end'])
+                e['display_period'] = f"{start_transformed} - {end_transformed}"
+
+                duration = compute_duration(e['start'], e['end'])
+
+                st.markdown(f"<span style='color:{LIGHT_GRAY}; font-weight:bold; white-space: pre;'>{e['institution']}</span>  | <span style='color:{LIGHT_GRAY};'>Grade: {e['grade']} | </span>  <span style='color:{LIGHT_GRAY};'>{e['display_period']} </span>", unsafe_allow_html=True)
+                st.markdown(f"### {e['name']}")
+                st.markdown(f"{e['description']}")
+                st.markdown(f"**Thesis title** {e['thesis']}")
+                
+                
+                st.markdown("<br>", unsafe_allow_html=True)
 
 
     
--- pages_script/projects.py
+++ pages_script/projects.py
@@ -8,30 +8,37 @@ st.set_page_config(
     layout='wide',
     initial_sidebar_state="expanded"
 )
-st.title('My projects')
 
 projects = read_json('projects.json')
 
 
 for idx, p in enumerate(projects):
-    with st.container(border=True):
-        
-        text_col, image_col = st.columns(2, gap='large', vertical_alignment='center')
+    #st.markdown('<br>', unsafe_allow_html=True)
+    with sc(key=f'pro_{idx}', css_styles=["""div[data-testid="stHorizontalBlock"]:first-of-type {
+                                        border: 1px solid #737373;
+                                        border-radius: 20px;
+                                        padding:15px;
+                                        box-shadow: rgb(0 0 0 / 20%) 0px 2px 1px -1px, rgb(0 0 0 / 14%) 0px 1px 1px 0px, rgb(0 0 0 / 12%) 0px 1px 3px 0px;
+    }
+    """]):
+        with st.container():
+            
+            text_col, image_col = st.columns(2, gap='large', vertical_alignment='center')
 
-        with text_col:
-            st.markdown(f"# {p['name']}")
-            tagger_component(
-                content='',
-                tags=p['tags']
-            )
-            st.markdown(p['description'])
-            st.link_button(p['button_label'], url=p['url'])
+            with text_col:
+                st.markdown(f"# {p['name']}")
+                tagger_component(
+                    content='',
+                    tags=p['tags']
+                )
+                st.markdown(p['description'])
+                st.link_button(p['button_label'], url=p['url'])
 
-        with image_col:
-            media = p['media']
-            if media['type'] == 'image':
-                st.image(media['path'])
-            else:
-                st.video(media['path'], loop=True, autoplay=True)
+            with image_col:
+                media = p['media']
+                if media['type'] == 'image':
+                    st.image(media['path'])
+                else:
+                    st.video(media['path'], loop=True, autoplay=True)
 
         
